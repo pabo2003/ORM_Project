b2ec92ee788592fd6a1130a89f034c27de2fdc35
Paboda Sanduni <sandunikhitigama@gmail.com>
2024-08-22 22:56:48 +0530

    first commit

--- src/main/java/lk/ijse/gdse/Main.java
+++ src/main/java/lk/ijse/gdse/Main.java
@@ -8,32 +8,32 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Session session = FactoryConfiguration.getInstance().getSession();
+        //open transaction
         Transaction transaction = session.beginTransaction();
 
-        // Student operations
-        Student student = new Student(1, new FullName("Paboda", "Sanduni"), "Kurunegala");
+        //Student save,delete,update
+        Student student = new Student(1,new FullName("Paboda","Sanduni"),"Kurunegala");
         session.save(student);
         session.delete(student);
+        session.update(student);
+        //get
         Student student1 = session.get(Student.class, 1);
-        if (student1 != null) {
-            System.out.println(student1.toString());
-        }
+        System.out.println(student1.toString());
+        //commit
         transaction.commit();
-        session.close();
 
-        // Teacher operations
+        //Teacher save ,delete,update
         Session session1 = FactoryConfiguration.getInstance().getSession();
         Transaction transaction1 = session1.beginTransaction();
-        Teacher teacher = new Teacher(2, "Kamala", "Kurunegala", 25000);
+        Teacher teacher = new Teacher(2,"Kamala","Kurunegala",25000);
         session1.save(teacher);
         session1.update(teacher);
         session1.delete(teacher);
-        Teacher teacher1 = session1.get(Teacher.class, 2);
-        if (teacher1 != null) {
-            System.out.println(teacher1.getName() + ", " + teacher1.getAddress() + ", " + teacher1.getSalary());
-        }
+        //get
+       Teacher teacher1= session1.get(Teacher.class, 1);
+        System.out.println(teacher1.getName() + ", " +teacher1.getAddress()+", "+teacher1.getSalary());
         transaction1.commit();
         session1.close();
     }
